a35707298c79c495cd34283099a6df12fe0af2f1
Aapo Rissanen <aapo.rissanen@myy.haaga-helia.fi>
2024-01-24 15:16:19 +0200

    Deleted unused imports

--- App.js
+++ App.js
@@ -1,15 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import {
-  Pressable,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  FlatList,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Calculator from "./.components/calculator";
